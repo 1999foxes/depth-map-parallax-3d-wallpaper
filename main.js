dec5b885266e1fd2eef57def218c06b02ca989a8
hl <hl1999@yeah.net>
2023-05-14 12:33:24 +0800

    resource auto fallback, use png if video is not found

--- main.js
+++ main.js
@@ -1,62 +1,77 @@
+// https://github.com/1999foxes
 import * as THREE from 'three';
 import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
 import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { ShaderPass } from 'three/addons/postprocessing/ShaderPass.js';
 
-// polyfill
+
+// Polyfill
 Number.prototype.clamp = function (min, max) {
   return Math.min(Math.max(this, min), max);
 };
 
+
 // Create a scene, camera and renderer
+
 var scene = new THREE.Scene();
+
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
 var renderer = new THREE.WebGLRenderer();
 var aspectRatio = 1080 / 1920;
 var width = Math.max(window.innerWidth, window.innerHeight / aspectRatio);
-renderer.setSize(width, width * aspectRatio); 
-console.log(width,window.innerWidth, window.innerHeight / aspectRatio);
+renderer.setSize(width, width * aspectRatio);
 document.body.appendChild(renderer.domElement);
 
-// Load the 2 textures
 
-// var textureLoader = new THREE.TextureLoader();
-// var texture1 = await textureLoader.loadAsync('test_images/noise.png');  // debug
-// var texture1 = await textureLoader.loadAsync('test_images/hutao.png');
-// var texture2 = await textureLoader.loadAsync('test_images/hutaodepth.png');
+// Load the 2 textures from video or png
 
-const video = document.getElementById('video');
-const texture1 = new THREE.VideoTexture(video);
+var textureLoader = new THREE.TextureLoader();
 
-const videoDepth = document.getElementById('videoDepth');
-const texture2 = new THREE.VideoTexture(videoDepth);
+var video = document.getElementById('video');
+const texture1 = video != null ?
+  new THREE.VideoTexture(video) :
+  await textureLoader.loadAsync('image.png');
+
+var videoDepth = document.getElementById('videoDepth');
+const texture2 = videoDepth != null ?
+  new THREE.VideoTexture(videoDepth) :
+  await textureLoader.loadAsync('depth.png');
 
 const audio = document.getElementById('audio');
 
-audio.onended = (event) => {
-  console.log("ended, looping.");
+function mediaPlay() {
+  if (video != null) {
+    video.pause();
+    video.currentTime = 0;
+    video.play();
+  }
 
-  video.pause();
-  videoDepth.pause();
-  audio.pause();
+  if (videoDepth != null) {
+    videoDepth.pause();
+    videoDepth.currentTime = 0;
+    videoDepth.play();
+  }
 
-  video.currentTime = 0;
-  videoDepth.currentTime = 0;
-  audio.currentTime = 0;
-
-  video.play();
-  videoDepth.play();
-  audio.play();
+  if (audio != null) {
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+  }
 }
 
-// Create a composer for post processing
+video && (video.onended = mediaPlay);
+videoDepth && (videoDepth.onended = mediaPlay);
+audio && (audio.onended = mediaPlay);
+
+
+// Set up post processing
+
 var composer = new EffectComposer(renderer);
 
-// Create a render pass and add it to the composer
 var renderPass = new RenderPass(scene, camera);
 composer.addPass(renderPass);
 
-// Create a shader pass with a custom shader and add it to the composer
 var shaderPass = new ShaderPass({
   uniforms: {
     tImage: { value: texture1 },
@@ -166,13 +181,17 @@ var shaderPass = new ShaderPass({
 shaderPass.renderToScreen = true; // make this pass the final output
 composer.addPass(shaderPass);
 
+
 // Create a plane geometry and material and add it to the scene
+
 var geometry = new THREE.PlaneGeometry(2, 2);
 var material = new THREE.MeshBasicMaterial({ color: 0xffffff });
 var plane = new THREE.Mesh(geometry, material);
 scene.add(plane);
 
+
 // Add an event listener for mouse move
+
 var mouseX = 0, mouseY = 0;
 window.addEventListener('mousemove', function (event) {
   // normalize the mouse position from -0.5 to 0.5
@@ -180,14 +199,16 @@ window.addEventListener('mousemove', function (event) {
   mouseY = 0.5 - (event.clientY / window.innerHeight);
 });
 
+
 // Animate the scene
+
 let clock = new THREE.Clock();
 let frameDelta = 0, frameInterval = 1 / 30;
 let x = mouseX, y = mouseY;
+
 function animate() {
   requestAnimationFrame(animate);
   const clockDelta = clock.getDelta();
-
   frameDelta += clockDelta;
   if (frameDelta > frameInterval) {
     x = mouseX * 0.2 + x * 0.8, y = mouseY * 0.2 + y * 0.8;
@@ -197,17 +218,18 @@ function animate() {
   }
 }
 
-var handlerFirstClick = function () {
+
+// Listen to first click event
+
+var firstClickHandler = function () {
   var click = 0;
   return function () {
     if (click === 0) {
-      video.play();
-      videoDepth.play();
-      audio.play();
+      mediaPlay();
       animate();
     }
     click++;
   }
 }();
 
-document.getElementById('cover').addEventListener("click", handlerFirstClick);
+document.getElementById('cover').addEventListener("click", firstClickHandler);
